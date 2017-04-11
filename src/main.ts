11747ae2dc8d411c29115046ffe97eb1c953f7ad
slikts <dabas@Untu.ms>
2017-04-12 02:04:11 +0300

    Release 1.0.5

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 declare function require(name:string): any;
 
-export interface Data {
-  [key: string]: string[]
-}
+type Data = { [key: string]: string[] }
 
 export const namedays = <Data>require('../data/namedays.json')
 export const extNamedays = <Data>require('../data/namedays-extended.json')
