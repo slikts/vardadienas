ece97d6f79a81651042f786320ec6b7a7606918a
slikts <dabas@Untu.ms>
2017-04-12 12:43:29 +0300

    Node 4 test

--- test/index.js
+++ test/index.js
@@ -1,1 +1,8 @@
-const { vd, ext } = require('../dist/main.js')
+var test = require('tape')
+var data = require('../dist/main.js')
+
+test('empty arg', (t) => {
+  t.ok(data.namedays)
+  t.ok(data.extNamedays)
+  t.end()
+})
